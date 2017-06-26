a486fe28f175f79e135ea34cd1c2bcab62878781
Ricardo Quesada <ricardoquesada@gmail.com>
2017-06-25 21:57:21 -0700

    testing 320x200x16 graphics mode

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
 	#nasm -fbin src/main.asm -o binexe.exe -i/Users/riq/src/nasm-2.13.01/misc/
 	nasm -f elf src/main.asm -o obj/main.o
-	smlrl -small obj/main.o -o mama.exe
+	smlrl -small obj/main.o -o mama.exe -map mama.map
 
 clean:
-	rm *.exe
+	rm *.exe *.map
 	rm obj/*
--- src/main.asm
+++ src/main.asm
@@ -18,9 +18,55 @@ __start:
         mov ds, ax ; DS=ES=SS in small model .EXEs and in tiny model .COMs
         mov es, ax
 
-        mov dx,hello
-        mov ah,9
-        int 0x21
+;        mov al, 0b0001_0000     ;40x25, text, color, bright
+;        mov dx, 0x3d8
+;        out dx, al
+
+        mov ax, 0x0009
+        int 0x10
+
+
+        mov cx, 8000                    ; bank #0
+        mov al, 0xee
+
+        mov bx, 0xb800
+        mov es, bx
+        xor di, di                      ; es:di: destination
+rep     stosb
+
+
+        mov cx, 8000                    ; bank #1
+        mov al, 0x12
+
+        mov bx, 0xb800
+        mov es, bx
+        mov di, 0x2000                  ; es:di: destination
+rep     stosb
+
+
+        mov cx, 8000                    ; bank #2
+        mov al, 0x34
+
+        mov bx, 0xb800
+        mov es, bx
+        mov di, 0x4000                  ; es:di: destination
+rep     stosb
+
+
+        mov cx, 8000                    ; bank #3
+        mov al, 0x56
+
+        mov bx, 0xb800
+        mov es, bx
+        mov di, 0x6000                  ; es:di: destination
+rep     stosb
+
+        xor  ah,ah                      ;Function number: get key
+        int  0x16                       ;Call BIOS keyboard interrupt
+
+;        mov dx,hello
+;        mov ah,9
+;        int 0x21
 
         mov ax,0x4c00
         int 0x21
