c299ffa319263ed2b5d316cb7b0e2d9c0979a8b3
Ricardo Quesada <ricardoquesada@gmail.com>
2017-09-13 23:07:20 -0700

    load song from command line

--- snd/pvmplayer.asm
+++ snd/pvmplayer.asm
@@ -1,8 +1,8 @@
 ; PVM (Play VGM Music) player
 ; Author: riq/pvm
 ;
-; PIC & IRQ init code: taken from tandysnd.asm by bisqwit@
-; PIC & IRQ fixes: by trixter@
+; PIC & IRQ init code: taken from tandysnd.asm by @bisqwit
+; PIC & IRQ fixes: by @trixter
 
 bits    16
 cpu     8086
@@ -18,16 +18,20 @@ section .text code
 ..start:
 
 main:
-        mov     ax,data                         ;init segments
-        mov     ds,ax                           ; DS=ES: same segment
-        mov     es,ax                           ; SS: stack
+        mov     ax,data                         ;init DS segment = data
+        mov     ds,ax                           ;es should not be modified
         mov     ax,stack
         cli                                     ;disable interrupts while
         mov     ss,ax                           ; setting the stack pointer
         mov     sp,stacktop
         sti
 
-        call    wait_key
+        mov     dx,msg_title
+        call    print_msg
+
+        call    parse_cmd_line                  ;es must remain intact until this
+
+        call    load_song
 
         call    music_init                      ;must be called before setup_irq
         call    setup_irq
@@ -37,24 +41,28 @@ main:
         call    restore_irq
         call    sound_cleanup
 
-        mov     ax,4C00h                        ;Terminate program
+        mov     ax,4c00h                        ;Terminate program
         int     21h                             ;INT 21, AH=4Ch, AL=exit code
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
-; Parametros
-; Chequea que los parametros de la linea sean correctos
-; ES:0081 -> empieza.
+print_msg:
+        mov     ah,9
+        int     21h
+        ret
+
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 parse_cmd_line:
         push ds
         push es
 
         push    ds                              ;swap ds,es
-        pop     es
+        push    es
         pop     ds
         pop     es
 
-        cld
+        cld                                     ;direction forward
+        stc                                     ;carry on (means error)
+
         mov     di,filename                     ;location for name
         mov     si,81h                          ;params are in ds:si
 
@@ -65,7 +73,8 @@ parse_cmd_line:
         cmp     al,13                           ;return?
         je      .exit                           ;if so, exit
 
-                                                ;otherwise it is the filename to read
+        clc                                     ;clear carry. means an argument
+                                                ; was passed
         stosb                                   ;write name in es:di
         jmp     .loop                           ; and keep reading
 
@@ -75,9 +84,59 @@ parse_cmd_line:
         mov     al,'$'                          ;to show filename
         stosb
 
-        pop  es
-        pop  ds
+        pop     es
+        pop     ds
+        jc      .error
         ret
+
+.error:
+        mov     dx,msg_help
+        call    print_msg
+        mov     ax,4c01h
+        int     21h
+
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+load_song:
+        push    ds
+
+        mov     dx,msg_loading
+        call    print_msg
+        mov     dx,filename
+        call    print_msg
+        mov     cx,msg_enter
+        call    print_msg
+
+        mov     ah,3dh                          ;open file
+        mov     al,0
+        mov     dx,filename
+        int     21h
+        jc      .error
+
+        mov     bx,ax                           ;file handle
+        mov     cx,0ffffh                       ;bytes to read: entire segment
+        xor     dx,dx
+        mov     ax,pvmsong
+        mov     ds,ax                           ;dst: pvmsong segment:0
+        mov     ah,3fh                          ;read file
+        int     21h
+        jc      .error
+
+        mov     ah,3eh                          ;close fd
+        int     21h
+        jc      .error                          ;error? exit
+
+        pop     ds
+        ret
+
+.error:
+        mov     dx,msg_error_load               ;print error message and exit
+        call    print_msg
+
+        pop     ds
+
+        mov     ax,4c02h
+        int     21h
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 setup_irq:
@@ -99,9 +158,9 @@ setup_irq:
         mov     ax,PIT_divider                  ;Configure the PIT to
         call    setup_PIT                       ;issue IRQ at 60 Hz rate
 
-        in      al,21h                          ;Read primary PIC IMR
-        mov     [PIC0IMR],al                    ;Store it for later
-        mov     al,11111100b                    ;Mask off everything except IRQ 0
+        in      al,21h                          ;Read primary PIC Interrupt Mask Register
+        mov     [old_pic_imr],al                ;Store it for later
+        mov     al,1111_1100b                   ;Mask off everything except IRQ 0
         out     21h,al                          ; and IRQ1 (timer and keyboard)
 
         sti
@@ -111,8 +170,8 @@ setup_irq:
 restore_irq:
         cli
 
-        mov     al,[PIC0IMR]                    ;Get old PIC settings
-        out     21h,al                          ;Set primary PIC IMR
+        mov     al,[old_pic_imr]                ;Get old PIC settings
+        out     21h,al                          ;Set primary PIC Interrupt Mask Register
 
         mov     ax,0                            ;Reset PIT to defaults (~18.2 Hz)
         call    setup_PIT                       ; actually means 10000h
@@ -130,7 +189,7 @@ restore_irq:
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 music_init:
-        mov     word [pvm_offset],pvm_song + 0x10       ;update start offset
+        mov     word [pvm_offset],10h           ;update start offset
         mov     byte [pvm_wait],0               ;don't wait at start
         ret
 
@@ -156,7 +215,7 @@ player_main:
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 wait_key:
         xor     ah,ah                           ;Function number: get key
-        int     0x16                            ;Call BIOS keyboard interrupt
+        int     16h                             ;Call BIOS keyboard interrupt
         ret
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
@@ -181,35 +240,38 @@ sound_cleanup:
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 song_tick:
 
-DATA    equ     0b0000_0000
-DATA_EXTRA equ  0b0010_0000
-DELAY   equ     0b0100_0000
-DELAY_EXTRA equ 0b0110_0000
-END     equ     0b1000_0000
+DATA    equ     0000_0000b
+DATA_EXTRA equ  0010_0000b
+DELAY   equ     0100_0000b
+DELAY_EXTRA equ 0110_0000b
+END     equ     1000_0000b
 
         int     3
         push    ax
         push    cx
         push    si
         push    ds
+        push    es
 
-        mov     ax,data
+        mov     ax,data                         ;vars in es
+        mov     es,ax
+        mov     ax,pvmsong                      ;song in ds
         mov     ds,ax
 
         sub     cx,cx                           ;cx=0... needed later
-        mov     si,[pvm_offset]
+        mov     si,[es:pvm_offset]
 
-        cmp     byte [pvm_wait],0
+        cmp     byte [es:pvm_wait],0
         je      .l0
 
-        dec     byte [pvm_wait]
+        dec     byte [es:pvm_wait]
         jmp     .exit
 
 .l0:
         lodsb                                   ;fetch command byte
         mov     ah,al
-        and     al,0b1110_0000                  ;al=command only
-        and     ah,0b0001_1111                  ;ah=command args only
+        and     al,1110_0000b                   ;al=command only
+        and     ah,0001_1111b                   ;ah=command args only
 
         cmp     al,DATA                         ;data?
         je      .is_data
@@ -234,37 +296,38 @@ END     equ     0b1000_0000
 
 .is_data_extra:
         lodsb                                   ;fetch lenght from next byte
-        mov     cl,al
+        mov     cl,al                           ;new repeat value taken from prev. fetch
 .repeat:
         lodsb
-        out     0xc0,al
+        out     0c0h,al
         loop    .repeat
 
-        jmp     .l0                             ; repeat... fetch next command
+        jmp     .l0                             ; start again. fetch next command
 
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 .is_delay:
         dec     ah                              ;minus one, since we are returning
-        mov     [pvm_wait],ah                   ; from here now
+        mov     [es:pvm_wait],ah                ; from here now
         jmp     .exit
 
 .is_delay_extra:
         lodsb                                   ;fetch wait from next byte
         dec     al                              ;minus one, since we are returning
-        mov     [pvm_wait],al                   ; from here now
+        mov     [es:pvm_wait],al                ; from here now
         jmp     .exit
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 .is_end:
-        mov     byte [pvm_wait], 5              ;wait 5 cycles before starting again
-        mov     word [pvm_offset], pvm_song + 0x10      ; beginning of song
+        mov     byte [es:pvm_wait],5            ;wait 5 cycles before starting again
+        mov     word [es:pvm_offset],10h        ; beginning of song
         jmp     .exit_skip
 
 .exit:
-        mov     [pvm_offset],si
+        mov     [es:pvm_offset],si
 .exit_skip:
 
+        pop     es
         pop     ds
         pop     si
         pop     cx
@@ -275,24 +338,22 @@ END     equ     0b1000_0000
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; IRQ
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
-new_i08:; New INT 08 (timer IRQ) handler
-
+new_i08:
         call    song_tick
 
         add     word [cs:i08_counter],PIT_divider
         jnc     skip_old_i08
-        db      0EAh                            ;Jump far
-old_i08:        dd 0                            ;Old INT 08 vector
-skip_old_i08:
+        db      0eah                            ;Jump far...
+old_i08:        dd 0                            ; ...to Old INT 08 vector
 
+skip_old_i08:
         push    ax
         mov     al,20h                          ;Send the EOI signal
-        out     20h,al                          ;to the IRQ controller
+        out     20h,al                          ; to the IRQ controller
         pop     ax
 
         iret                                    ;Exit interrupt
 
-i08_counter:    dw 0
 ; I08counter makes it possible to call the
 ; the old IRQ vector at the right rate.
 ; At every INT, it is incremented by:
@@ -303,31 +364,44 @@ i08_counter:    dw 0
 ; it's time to call the old IRQ handler.
 ; This ensures that the old IRQ handler is
 ; called at the standard 18.2 Hz rate.
+i08_counter:
+        dw      0
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; section DATA
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 section .data data
 
+;messages
+msg_title:      db 'pvmplayer v0.1 - riq/pvm',13,10,'$'
+msg_help:       db 'usage:',13,10
+                db '   pvmplayer songname.pvm',13,13,'$'
+msg_loading:    db 'loading $'
+msg_error_load: db 'error loading',13,10,'$'
+msg_enter:      db 13,10
+
+;vars
 filename:
-        resb    64                              ; 64 bytes for the name
+        resb    64                              ;64 bytes for the name
 
-pvm_song:
-;        incbin "music_test.pvm"
-;        incbin "anime.pvm"
-        incbin "short-lived.pvm"
+pvm_wait:                                       ;cycles wait
+        db      0
 
-pvm_wait:                                       ;cycles to read diviced 0x2df
-        db 0
 pvm_offset:                                     ;pointer to next byte to read
-        dw 0
+        dw      0
 
-PIC0IMR:
-        db 0                                    ;PICOIMR original value
+old_pic_imr:
+        db      0                               ;PIC IMR original value
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; section STACK
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+section .pvmsong data
+        resb    65536
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; section STACK
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 section .stack stack
-        resb 4096
+        resb    1024
 stacktop:
